39677ea64b2101ad2906b750bde3c7eb5ac9a305
Trevor Power <37224494+trevorjpower@users.noreply.github.com>
2024-04-30 17:28:37 +0100

    Added day1.py code for band name generator - test pass

---
+++ day-1-10/day1.py
@@ -0,0 +1,19 @@
+# Day 1 
+
+# Strings, variables, and input
+
+# Band name generator
+
+# Print a welcome message to the user
+print("Welcome to the Band Name Generator.\n")
+
+# Ask the user for the city they grew up in
+city = input("What's the name of the city you grew up in?\n")
+
+# Ask the user for the name of a pet
+pet = input("What's your pet's name?\n")
+
+# Print the name of their band
+print(f"Your band name could be {city} {pet}.\n")
+
+# Output: Pass
